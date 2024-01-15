212d30117a79e71eb21ae7a86c73ed115f87e064
Nursultan Dulatov <80988817+Twoeyes13@users.noreply.github.com>
2024-01-16 00:41:12 +0600

    Test for compile!

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,14 @@
 package com.epam.demo;
 
 import java.util.List;
+import com.epam.utils.StringUtils;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
+        for (String el : args) {
+            if(StringUtils.isPositiveNumber(el) == false) return false;
+        }
        //magic happens here
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,5 +3,11 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         //code!
+        if (str == null || str.isEmpty())  return false;
+        if (str.matches("-?\\d+(\\.\\d+)?")) {
+            if (Double.parseDouble(str) < 1) return false;
+            if (str.substring(0, 1).equals("0") && !str.substring(1, 2).equals(".")) return false;
+        } else return false;
+        return true;
     }
 }
